0b45d7512ba5c875d9ce3f4a1c540fc1f09d644f
tianmingfu <tianmf@teld.cn>
2025-12-31 15:50:50 +0800

    computed-catch实现

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -58,9 +58,23 @@ export function trigger(target: object, key?: unknown) {
  * 依次触发 dep 中保存的依赖
  */
 export function triggerEffects(dep: Dep) {
+  // 把 dep 构建为一个数组
   const effects = isArray(dep) ? dep : [...dep]
+  // 依次触发
+  // for (const effect of effects) {
+  // 	triggerEffect(effect)
+  // }
+
+  // 不在依次触发，而是先触发所有的计算属性依赖，再触发所有的非计算属性依赖
   for (const effect of effects) {
+    if (effect.computed) {
       triggerEffect(effect)
+    }
+  }
+  for (const effect of effects) {
+    if (!effect.computed) {
+      triggerEffect(effect)
+    }
   }
 }
 

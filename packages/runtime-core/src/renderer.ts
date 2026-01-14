2e0d754e2b085468b381b016b67ce341bb62ec06
tianmingfu <tianmf@teld.cn>
2026-01-14 15:42:56 +0800

    render函数实现1

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -1,4 +1,354 @@
+import { EMPTY_OBJ, isString } from '@vue/shared'
+import { ReactiveEffect } from 'packages/reactivity/src/effect'
+import { ShapeFlags } from 'packages/shared/src/shapeFlags'
+import { Comment, Fragment, isSameVNodeType, Text } from './vnode'
 
-export function createRenderer() {
+/**
+ * 渲染器配置对象
+ */
+export interface RendererOptions {
+  /**
+   * 为指定 element 的 prop 打补丁
+   */
+  patchProp(el: Element, key: string, prevValue: any, nextValue: any): void
+  /**
+   * 为指定的 Element 设置 text
+   */
+  setElementText(node: Element, text: string): void
+  /**
+   * 插入指定的 el 到 parent 中，anchor 表示插入的位置，即：锚点
+   */
+  insert(el, parent: Element, anchor?): void
+  /**
+   * 创建指定的 Element
+   */
+  createElement(type: string)
+  /**
+   * 卸载指定dom
+   */
+  remove(el): void
+  /**
+   * 创建 Text 节点
+   */
+  createText(text: string)
+  /**
+   * 设置 text
+   */
+  setText(node, text): void
+  /**
+   * 设置 text
+   */
+  createComment(text: string)
+}
+/**
+ * 对外暴露的创建渲染器的方法
+ */
+export function createRenderer(options: RendererOptions) {
+  return baseCreateRenderer(options)
+}
+
+
+/**
+ * 生成 renderer 渲染器
+ * @param options 兼容性操作配置对象
+ * @returns
+ */
+function baseCreateRenderer(options: RendererOptions): any {
+  /**
+   * 解构 options，获取所有的兼容性方法
+   */
+  const {
+    insert: hostInsert,
+    patchProp: hostPatchProp,
+    createElement: hostCreateElement,
+    setElementText: hostSetElementText,
+    remove: hostRemove,
+    createText: hostCreateText,
+    setText: hostSetText,
+    createComment: hostCreateComment
+  } = options
+
+  /**
+   * Comment 的打补丁操作
+   */
+  const processCommentNode = (oldVNode, newVNode, container, anchor) => {
+    if (oldVNode == null) {
+      // 生成节点
+      newVNode.el = hostCreateComment((newVNode.children as string) || '')
+      // 挂载
+      hostInsert(newVNode.el, container, anchor)
+    } else {
+      // 无更新
+      newVNode.el = oldVNode.el
+    }
+  }
+
+  /**
+   * Text 的打补丁操作
+   */
+  const processText = (oldVNode, newVNode, container, anchor) => {
+    // 不存在旧的节点，则为 挂载 操作
+    if (oldVNode == null) {
+      // 生成节点
+      newVNode.el = hostCreateText(newVNode.children as string)
+      // 挂载
+      hostInsert(newVNode.el, container, anchor)
+    }
+    // 存在旧的节点，则为 更新 操作
+    else {
+      const el = (newVNode.el = oldVNode.el!)
+      if (newVNode.children !== oldVNode.children) {
+        hostSetText(el, newVNode.children as string)
+      }
+    }
+  }
+
+  /**
+   * Element 的打补丁操作
+   */
+  const processElement = (oldVNode, newVNode, container, anchor) => {
+    if (oldVNode == null) {
+      // 挂载操作
+      mountElement(newVNode, container, anchor)
+    } else {
+      // 更新操作
+      patchElement(oldVNode, newVNode)
+    }
+  }
+
+  /**
+   * Fragment 的打补丁操作
+   */
+  const processFragment = (oldVNode, newVNode, container, anchor) => {
+    if (oldVNode == null) {
+      mountChildren(newVNode.children, container, anchor)
+    } else {
+      patchChildren(oldVNode, newVNode, container, anchor)
+    }
+  }
+
+  /**
+   * 组件的打补丁操作
+   */
+  const processComponent = (oldVNode, newVNode, container, anchor) => {
+    if (oldVNode == null) {
+      // 挂载
+      mountComponent(newVNode, container, anchor)
+    }
+  }
+
+  const mountComponent = (initialVNode, container, anchor) => {
     
+  }
+
+  /**
+   * element 的更新操作
+   */
+  const patchElement = (oldVNode, newVNode) => {
+    // 获取指定的 el
+    const el = (newVNode.el = oldVNode.el!)
+
+    // 新旧 props
+    const oldProps = oldVNode.props || EMPTY_OBJ
+    const newProps = newVNode.props || EMPTY_OBJ
+
+    // 更新子节点
+    patchChildren(oldVNode, newVNode, el, null)
+
+    // 更新 props
+    patchProps(el, newVNode, oldProps, newProps)
+  }
+
+  /**
+   * element 的挂载操作
+   */
+  const mountElement = (vnode, container, anchor) => {
+    const { type, props, shapeFlag } = vnode
+
+    // 创建 element
+    const el = (vnode.el = hostCreateElement(type))
+
+    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      // 设置 文本子节点
+      hostSetElementText(el, vnode.children as string)
+    } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+      // 设置 Array 子节点
+      mountChildren(vnode.children, el, anchor)
+    }
+
+    // 处理 props
+    if (props) {
+      // 遍历 props 对象
+      for (const key in props) {
+        hostPatchProp(el, key, null, props[key])
+      }
+    }
+
+    // 插入 el 到指定的位置
+    hostInsert(el, container, anchor)
+  }
+
+  /**
+   * 为 props 打补丁
+   */
+  const patchProps = (el: Element, vnode, oldProps, newProps) => {
+    // 新旧 props 不相同时才进行处理
+    if (oldProps !== newProps) {
+      // 遍历新的 props，依次触发 hostPatchProp ，赋值新属性
+      for (const key in newProps) {
+        const next = newProps[key]
+        const prev = oldProps[key]
+        if (next !== prev) {
+          hostPatchProp(el, key, prev, next)
+        }
+      }
+      // 存在旧的 props 时
+      if (oldProps !== EMPTY_OBJ) {
+        // 遍历旧的 props，依次触发 hostPatchProp ，删除不存在于新props 中的旧属性
+        for (const key in oldProps) {
+          if (!(key in newProps)) {
+            hostPatchProp(el, key, oldProps[key], null)
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * 挂载子节点
+   */
+  const mountChildren = (children, container, anchor) => {
+
+  }
+
+  /**
+   * 为子节点打补丁
+   */
+  const patchChildren = (oldVNode, newVNode, container, anchor) => {
+    // 旧节点的 children
+    const c1 = oldVNode && oldVNode.children
+    // 旧节点的 prevShapeFlag
+    const prevShapeFlag = oldVNode ? oldVNode.shapeFlag : 0
+    // 新节点的 children
+    const c2 = newVNode.children
+
+    // 新节点的 shapeFlag
+    const { shapeFlag } = newVNode
+
+    // 新子节点为 TEXT_CHILDREN
+    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      // 旧子节点为 ARRAY_CHILDREN
+      if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+        // TODO: 卸载旧子节点
+      }
+      // 新旧子节点不同
+      if (c2 !== c1) {
+        // 挂载新子节点的文本
+        hostSetElementText(container, c2 as string)
+      }
+    } else {
+      // 旧子节点为 ARRAY_CHILDREN
+      if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+        // 新子节点也为 ARRAY_CHILDREN
+        if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+          // 这里要进行 diff 运算
+          patchKeyedChildren(c1, c2, container, anchor)
+        }
+        // 新子节点不为 ARRAY_CHILDREN，则直接卸载旧子节点
+        else {
+          // TODO: 卸载
+        }
+      } else {
+        // 旧子节点为 TEXT_CHILDREN
+        if (prevShapeFlag & ShapeFlags.TEXT_CHILDREN) {
+          // 删除旧的文本
+          hostSetElementText(container, '')
+        }
+        // 新子节点为 ARRAY_CHILDREN
+        if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+          // TODO: 单独挂载新子节点操作
+        }
+      }
+    }
+  }
+
+  /**
+   * diff
+   */
+  const patchKeyedChildren = (
+    oldChildren,
+    newChildren,
+    container,
+    parentAnchor
+  ) => {
+  }
+
+  /**
+   * 移动节点到指定位置
+   */
+  const move = (vnode, container, anchor) => {
+    const { el } = vnode
+    hostInsert(el!, container, anchor)
+  }
+
+  const patch = (oldVNode, newVNode, container, anchor = null) => {
+    if (oldVNode === newVNode) {
+      return
+    }
+
+    /**
+     * 判断是否为相同类型节点
+     */
+    if (oldVNode && !isSameVNodeType(oldVNode, newVNode)) {
+      unmount(oldVNode)
+      oldVNode = null
+    }
+
+    const { type, shapeFlag } = newVNode
+    switch (type) {
+      case Text:
+        // Text
+        processText(oldVNode, newVNode, container, anchor)
+        break
+      case Comment:
+        // Comment
+        processCommentNode(oldVNode, newVNode, container, anchor)
+        break
+      case Fragment:
+        // Fragment
+        processFragment(oldVNode, newVNode, container, anchor)
+        break
+      default:
+        if (shapeFlag & ShapeFlags.ELEMENT) {
+          processElement(oldVNode, newVNode, container, anchor)
+        } else if (shapeFlag & ShapeFlags.COMPONENT) {
+          // 组件
+          processComponent(oldVNode, newVNode, container, anchor)
+        }
+    }
+  }
+
+  const unmount = vnode => {
+    hostRemove(vnode.el!)
+  }
+
+  /**
+   * 渲染函数
+   */
+  const render = (vnode, container) => {
+    if (vnode == null) {
+      // 卸载
+      if (container._vnode) {
+        unmount(container._vnode)
+      }
+    } else {
+      // 打补丁（包括了挂载和更新）
+      patch(container._vnode || null, vnode, container)
+    }
+    container._vnode = vnode
+  }
+
+  return {
+    render
+  }
 }
